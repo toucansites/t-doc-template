fc6d94a67461478e4608e7a0fe5e4502c14d23dc
GErP83 <gurrka@gmail.com>
2025-10-17 10:57:05 +0200

    more changes for redesign

--- assets/javascript/navigation-active.js
+++ assets/javascript/navigation-active.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
   const currentPath = window.location.pathname.toLowerCase();
 
   links.forEach(link => {
-      const linkText = link.textContent.trim().toLowerCase();
+    const linkText = link.textContent.trim().toLowerCase();
+    if (currentPath != "/404.html") {
       if (currentPath.includes(linkText)) {
         link.classList.add('active');
-      } else if (currentPath == "/" && linkText == "docs") {
+      } else if (linkText == "docs" && currentPath != "/license/") {
         link.classList.add('active');
       }
+    } 
   });
 
 });
